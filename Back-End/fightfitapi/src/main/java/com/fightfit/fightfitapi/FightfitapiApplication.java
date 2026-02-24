244c254c766e942bb0624a6d011a3dc94168ee82
Carlos Eduardo <melocaduzinho@gmail.com>
2026-02-24 01:48:34 -0300

    Inicio do Projeto Pessoal FightFit

---
+++ Back-End/fightfitapi/src/main/java/com/fightfit/fightfitapi/FightfitapiApplication.java
@@ -0,0 +1,13 @@
+package com.fightfit.fightfitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightfitapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightfitapiApplication.class, args);
+	}
+
+}
---
+++ Back-End/fightfitapi/src/test/java/com/fightfit/fightfitapi/FightfitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fightfit.fightfitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightfitapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
